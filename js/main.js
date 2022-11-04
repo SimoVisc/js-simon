442095d1ce1d17de9905766309f6a8de9df73be3
SimoVisc <simonaviscardivs@gmail.com>
2022-11-04 16:11:54 +0100

    Sistema ciclo while

--- js/main.js
+++ js/main.js
@@ -6,15 +6,16 @@ function getRndInteger (min, max) {
 
 // creo un array di 5 numeri da 1 a 100 senza duplicati
 const listNumbers= [];
-while (listNumbers.length<= 5){
-   const numberRandom = getRndIteger(1,100)
+while (listNumbers.length<5){
+   const numberRandom = getRndInteger(1,100)
   // se il numero non è presente nell'arrey allora lo pusho
   if(listNumbers.includes(numberRandom)=== false){
     listNumbers.push(numberRandom)
   }
 }
+document.getElementById("simonSays").innerHTML=  listNumbers
 
-Visualizzare in pagina 5 numeri casuali da 1 a 100 senza duplicati.
-Da lì parte un timer di 30 secondi.
-Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
